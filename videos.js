32457eb913280a80a4db3989aab341e21a6c77e9
Segej-x7 <112220404+Segej-x7@users.noreply.github.com>
2025-03-20 18:21:54 +0300

    1
    1

---
+++ videos.js
@@ -0,0 +1,22 @@
+const videos = [
+    {
+        title: "Урок № 1",
+        src: "https://drive.google.com/file/d/1xkLGKB-aVaCkAw11GlNGkilp_1Hw-zcp/preview"
+    },
+    {
+        title: "Урок № 2",
+        src: "https://drive.google.com/file/d/1ip0cTRCQjRIXYMe-Lzm2JdUdErDVgUw0/preview"
+    },
+    {
+        title: "Урок № 3",
+        src: "https://drive.google.com/file/d/1RBJ_gdep7mp0ocILyr_h1xw-LEhoa1pt/preview"
+    },
+    {
+        title: "Урок № 4",
+        src: "https://drive.google.com/file/d/1GwO-Lh1WtAaWplV9TXD-tPtFE-erX2ra/preview"
+    },
+    {
+        title: "Урок № 5",
+        src: "https://drive.google.com/file/d/1jvURnTjz73bcj5pXNk_NEzcG11K3iYUE/preview"
+    }
+];
